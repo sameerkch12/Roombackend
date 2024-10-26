fef9bb902f0829d39d7b8ce2e3580b3d6e6c61c6
niteshogrey <ogreynitesh1102@gmail.com>
2024-10-26 20:58:48 +0530

    update-hotel

--- src/routes/hoteladmin.ts
+++ src/routes/hoteladmin.ts
@@ -1,11 +1,11 @@
 import express, { Router } from "express";
-import { createHotel, getHotels } from "../controllers/hoteladmin";
-import upload from "../config/uploadConfig";
+import { createHotel, getHotels } from "../controllers/hotelController";
+import upload from "../middlewares/multer";
 
 const router: Router = express.Router();
 
 // Route for creating a hotel with image upload (max 5 images)
-router.post('/create', upload.array('images', 5), createHotel);
+router.post('/create', upload.array('images', 5), createHotel); //http:localhost:3000/api/hoteladmin/create
 
 // Route for getting all hotels
 router.get('/', getHotels);
